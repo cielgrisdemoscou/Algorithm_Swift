b4979ee48656b1f9b5cbcc0c6fcb940be146f4de
Ted <50784573+cielgrisdemoscou@users.noreply.github.com>
2019-08-02 23:09:51 +0900

    Add files via upload

---
+++ String/412. Fizz Buzz.swift
@@ -0,0 +1,27 @@
+/**
+ * Question Link: https://leetcode.com/problems/fizz-buzz/
+ */
+
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var res = [String]()
+    
+        if n < 0 {
+            return res
+        }
+        
+        for i in 1...n {
+            if i % 3 == 0 && i % 5 == 0 {
+                res.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                res.append("Fizz")
+            } else if i % 5 == 0 {
+                res.append("Buzz")
+            } else {
+                res.append("\(i)")
+            }
+        }
+        
+        return res
+    }
+}
