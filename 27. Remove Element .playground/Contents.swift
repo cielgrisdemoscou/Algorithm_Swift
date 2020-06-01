f59f8224035a3b175bac1296d07996dd65538893
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-06-01 18:38:39 +0900

    #2020.06.01

---
+++ 27. Remove Element .playground/Contents.swift
@@ -0,0 +1,16 @@
+/*
+* Question Link: https://leetcode.com/problems/remove-element/
+*/
+
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var i = 0
+        for j in 0 ..< nums.count {
+            if nums[j] != val {
+                nums[i] = nums[j]
+                i += 1
+            }
+        }
+        return i 
+    }
+}
