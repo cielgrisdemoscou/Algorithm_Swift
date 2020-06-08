82190d727b19e93e90350bccaf226233d125964f
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-06-08 16:01:55 +0900

    #2020.06.08

---
+++ 14. Longest Common Prefix.playground/Contents.swift
@@ -0,0 +1,27 @@
+/*
+* Question Link: https://leetcode.com/problems/longest-common-prefix/
+*/
+
+
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        if strs.isEmpty { return "" }
+        var minLen = Int.max
+        var result: [[Character]] = []
+        for str in strs {
+            let str = Array(str)
+            minLen = min(minLen, str.count)
+            if !result.isEmpty {
+                for i in 0..<minLen {
+                    if !result.isEmpty && result.first![i] != str[i] {
+                        minLen = i
+                        break
+                    }
+                }
+            }
+            if minLen == 0 { return "" }
+            result.append(str)
+        }
+        return String(result[0][0..<minLen])
+    }
+}
